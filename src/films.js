13047eb8f13c23d104382953adb92a27f6aeae5d
patriciobevaqua <patriciobq@gmail.com>
2022-05-13 19:27:08 +0200

    suboEjercicio1

--- src/films.js
+++ src/films.js
@@ -1,6 +1,6 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
+  let result =  array.map( x => x.director);
   console.log("EXERCICE 1 ->", result);
   return result;
 }
